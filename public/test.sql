ce248e385c1e434896a394ee57277f6af2e5ed31
Kai <cgk0910@gmail.com>
2025-03-04 15:20:53 +0800

    create

--- public/test.sql
+++ public/test.sql
@@ -32,4 +32,18 @@ VALUES
     (NULL, 'amy', '0911-111-111'),
     (NULL, 'bob', '0922'),
     (NULL, 'cat', '0933'),
-    (NULL, 'dog', '0944')
+    (NULL, 'dog', '0944');
+
+INSERT INTO
+    `students` (
+        `id`,
+        `name`,
+        `mobile`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES
+    (NULL, 'amy', '0911', NULL, NULL),
+    (NULL, 'bob', '0922', NULL, NULL),
+    (NULL, 'cat', '0933', NULL, NULL),
+    (NULL, 'dog', '0944', NULL, NULL);
